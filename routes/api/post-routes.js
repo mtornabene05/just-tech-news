4a32f80407ae38f948d42018987b4640a5e326af
Maria <mtornabene05@gmail.com>
2022-03-13 19:07:16 -0400

    put route created for voting on a post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
+const { sequelize } = require("../../models/User");
 
 //get all users
 router.get("/", (req, res) => {
@@ -59,6 +60,38 @@ router.post("/", (req, res) => {
         .catch((err) => {
             console.log(err);
             res.status(500).json(err);
+        });
+});
+
+//PUT /api/posts/upvote
+router.put("/upvote", (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id,
+    })
+        .then(() => {
+            return Post.findOne({
+                where: {
+                    id: req.body.post_id,
+                },
+                attributes: [
+                    "id",
+                    "post_url",
+                    "title",
+                    "created_at",
+                    [
+                        sequelize.literal(
+                            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+                        ),
+                        "vote_count",
+                    ],
+                ],
+            });
+        })
+        .then((dbPostData) => res.json(dbPostData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
         });
 });
 
